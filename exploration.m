30adc95c5133d5896fed9c36cf66094232d02fe3
Benyamin-abdollahzadeh <86470404+Benyamin-abdollahzadeh@users.noreply.github.com>
2021-08-13 10:55:55 +0430

    Update exploration.m

--- exploration.m
+++ exploration.m
@@ -1,7 +1,7 @@
 function [current_vulture_X] = exploration(current_vulture_X, random_vulture_X, F, p1, upper_bound, lower_bound)
 
     if rand<p1
-        current_vulture_X=random_vulture_X-(abs(2*rand)*random_vulture_X-current_vulture_X)*F;
+        current_vulture_X=random_vulture_X-(abs((2*rand)*random_vulture_X-current_vulture_X))*F;
     else
         current_vulture_X=(random_vulture_X-(F)+rand()*((upper_bound-lower_bound)*rand+lower_bound));
     end
